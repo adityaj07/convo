f4d5457280b62a131b7fd51e06a7dc73b4f0de3b
Aditya Joshi <joshiaditya.aj07@gmail.com>
2025-07-07 19:33:34 +0530

    feat: add schemas and types packages

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,6 +4,9 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 
+import { userLoginSchema } from "@convo/schemas/auth";
+import { type userLoginSchemaT } from "@convo/types";
+
 const app = new Hono();
 
 app.use(logger());
